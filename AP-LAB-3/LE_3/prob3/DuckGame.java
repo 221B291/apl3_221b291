15c75b3d925ba0cb9cc47071d551f387f3ac71e5
221B291 <221B291@juet.co.in>
2025-03-26 15:36:51 +0530

    4th push

--- AP-LAB-3/LE_3/prob3/DuckGame.java
+++ AP-LAB-3/LE_3/prob3/DuckGame.java
@@ -5,22 +5,22 @@ public class DuckGame {
         RedHeadDuck redHeadDuck = new RedHeadDuck();
         LakeDuck lakeDuck = new LakeDuck();
 
-        // Test Rubber Duck
+     
         System.out.println("Rubber Duck:");
         rubberDuck.swim();
         rubberDuck.squeak();
         
-        // Test Wooden Duck
+        
         System.out.println("\nWooden Duck:");
         woodenDuck.swim();
         
-        // Test RedHead Duck
+    
         System.out.println("\nRedHead Duck:");
         redHeadDuck.swim();
         redHeadDuck.fly();
         redHeadDuck.quack();
         
-        // Test Lake Duck
+    
         System.out.println("\nLake Duck:");
         lakeDuck.swim();
         lakeDuck.fly();
